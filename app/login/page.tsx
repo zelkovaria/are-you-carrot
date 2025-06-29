8463806088f3c3ffb2f3de069e4e3a5480af974b
zelkovaria <hyein396@gmail.com>
2025-06-29 09:40:14 +0900

    feat: Login page 구현 및 컴포넌트 분리

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,29 +2,22 @@ import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
-export default function CreateAccount() {
+export default function LogIn() {
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요</h1>
-        <h2 className="text-xl">Fill in the form below to join!</h2>
+        <h2 className="text-xl">Log in with email and password</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput
-          type="text"
-          placeholder="username"
-          required
-          error={["user name is too short"]}
-        />
         <FormInput type="email" placeholder="email" required error={[]} />
-        <FormInput type="password" placeholder="password" required error={[]} />
         <FormInput
           type="password"
           placeholder="confirm password"
           required
           error={[]}
         />
-        <FormButton loading={false} text="create account" />
+        <FormButton loading={false} text="login" />
       </form>
       <SocialLogin />
     </div>
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -12,7 +12,7 @@ export default function FormInput({
   error,
 }: FormInputProps) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 *:pl-2">
       <input
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
         type={type}
